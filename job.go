474307b6db1dccd3434de3c873ed46a05e3a1b47
Tomas Zahradnik <tomas.zahradnik@kiwi.com>
2017-10-17 22:05:26 +0200

    job: add basic job execution

--- job.go
+++ job.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -12,12 +14,39 @@ type Job struct {
 	Expression string    // crontab line expression
 	Scheduled  time.Time // start date
 	Payload    []byte    // TODO: limit size
+
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+// NOTE: not safe to call concurrently
+func (j *Job) Init(ctx context.Context) {
+	ctxNew, cancel := context.WithCancel(ctx)
+	j.ctx = ctxNew
+	j.cancel = cancel
 }
 
 func (j *Job) Key() string {
 	return ""
 }
 
-func (j *Job) Run()     {}
-func (j *Job) runCron() {}
-func (j *Job) Cancel()  {}
+func (j *Job) Start() {
+	select {
+	case <-time.After(time.Until(j.Scheduled)):
+		if err := j.ping(); err != nil {
+			// TODO: job failed
+		}
+	case <-j.ctx.Done():
+		// TODO: log exit
+	}
+
+}
+
+func (j *Job) Cancel() {
+	j.cancel()
+}
+
+func (j *Job) ping() error {
+	fmt.Println("runnig job at:", time.Now(), "scheduled:", j.Scheduled, "")
+	return nil
+}
